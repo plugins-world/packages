119c3edef40fe4dafc4be7d412e19340beacf225
mouyong <my24251325@gmail.com>
2023-03-08 20:36:01 +0800

    chore: 迁移 LaravelDoc

--- bin/split.sh
+++ bin/split.sh
@@ -29,6 +29,8 @@ git pull origin $CURRENT_BRANCH || true
 #remote foundation git@github.com:mouyong/laravel-foundation.git
 remote MarketManager git@github.com:mouyong/MarketManager.git
 remote LaravelConfig git@github.com:mouyong/laravel-config.git
+remote LaravelDoc git@github.com:mouyong/laravel-doc.git
+
 remote DcatSaas git@github.com:mouyong/dcat-saas.git
 remote LaravelJwtAuth git@github.com:mouyong/laravel-jwt-auth.git
 remote LaravelQiNiu git@github.com:mouyong/laravel-qiniu.git
@@ -36,6 +38,8 @@ remote LaravelQiNiu git@github.com:mouyong/laravel-qiniu.git
 #split 'src/Illuminate/Foundation' foundation
 split 'MarketManager' MarketManager
 split 'LaravelConfig' LaravelConfig
+split 'LaravelDoc' LaravelDoc
+
 split 'DcatSaas' DcatSaas
 split 'LaravelJwtAuth' LaravelJwtAuth
 split 'LaravelQiNiu' LaravelQiNiu
