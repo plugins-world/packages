9b40c8d75fa5de77495be35dd954d7c99119b508
mouyong <my24251325@gmail.com>
2023-03-08 20:14:59 +0800

    chore: update permission

--- bin/split.sh
+++ bin/split.sh
@@ -5,9 +5,14 @@ set -x
 
 CURRENT_BRANCH="master"
 
+SPLITSH_LITE_BIN=splitsh-lite
+if [ ! -x $SPLITSH_LITE_BIN ]; then
+    SPLITSH_LITE_BIN=./bin/splitsh-lite
+fi
+
 function split()
 {
-    SHA1=`./splitsh-lite --prefix=$1`
+    SHA1=`${SPLITSH_LITE_BIN} --prefix=$1`
     git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
     git remote remove $2 || true
     git branch -D $2 || true
@@ -22,7 +27,9 @@ git pull origin $CURRENT_BRANCH || true
 
 
 #remote foundation git@github.com:mouyong/laravel-foundation.git
+remote MarketManager git@github.com:mouyong/MarketManager.git
 remote LaravelConfig git@github.com:mouyong/laravel-config.git
 
 #split 'src/Illuminate/Foundation' foundation
+split 'MarketManager' MarketManager
 split 'LaravelConfig' LaravelConfig
