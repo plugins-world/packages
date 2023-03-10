ea58a3dc70a0dbb82702700c641eecf31647b80e
mouyong <my24251325@gmail.com>
2023-03-10 08:19:46 +0800

    chore: github actions

--- bin/subtree-split.sh
+++ bin/subtree-split.sh
@@ -24,3 +24,20 @@ git pull origin $CURRENT_BRANCH || true
 # remote dcat-saas git@github.com:mouyong/dcat-saas.git
 
 # split 'extensions/plugins/DcatSaas' dcat-saas
+remote MarketManager git@github.com:plugins-world/MarketManager.git
+
+remote LaravelOauth git@github.com:mouyong/laravel-oauth.git
+remote LaravelConfig git@github.com:mouyong/laravel-config.git
+remote LaravelDoc git@github.com:mouyong/laravel-doc.git
+remote PhpSupport git@github.com:mouyong/php-support.git
+remote Translate git@github.com:mouyong/translate.git
+remote ECloudHousekeeper git@github.com:mouyong/ECloudHousekeeper.git
+
+split 'MarketManager' MarketManager
+
+split 'LaravelOauth' LaravelOauth
+split 'LaravelConfig' LaravelConfig
+split 'LaravelDoc' LaravelDoc
+split 'PhpSupport' PhpSupport
+split 'Translate' Translate
+split 'ECloudHousekeeper' ECloudHousekeeper
