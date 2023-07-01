62895803696fc65fce910ec38d9dedfc4fc60036
mouyong <my24251325@gmail.com>
2023-07-01 16:24:44 +0800

    refactor: remove db_config

--- LaravelConfig/app/helpers.php
+++ LaravelConfig/app/helpers.php
@@ -1,41 +1,16 @@
 <?php
 
-use Plugins\LaravelConfig\Models\Config;
-
-if (! function_exists('db_config')) {
-    function db_config($itemKey = null): mixed
+if (! function_exists('is_tenant_mode')) {
+    function is_tenant_mode(): mixed
     {
-        $configModel = config('laravel-config.config_model', Config::class);
-        
-        if (is_string($itemKey)) {
-            return $configModel::getItemValueByItemKey($itemKey);
-        }
-
-        if (is_array($itemKey)) {
-            return $configModel::getValueByKeys($itemKey);
-        }
-
-        return new $configModel();
-    }
-}
-
-if (! function_exists('db_config_central')) {
-    function db_config_central($itemKey = null): mixed
-    {
-        if (! function_exists('tenancy')) {
-            return db_config($itemKey);
-        }
-
-        return central(function ($tenant) use ($itemKey) {
-            return db_config($itemKey);
-        });
+        return function_exists('tenancy');
     }
 }
 
 if (! function_exists('central')) {
     function central(callable $callable): mixed
     {
-        if (! function_exists('tenancy')) {
+        if (! is_tenant_mode()) {
             $tenant = null;
 
             return $callable($tenant);
