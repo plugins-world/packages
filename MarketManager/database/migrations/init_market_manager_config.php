1a724792d91c07c9a0587298bbfc12037cd508ec
mouyong <my24251325@gmail.com>
2023-07-03 01:55:31 +0800

    fix: 修复无法安装插件管理器的问题

--- MarketManager/database/migrations/init_market_manager_config.php
+++ MarketManager/database/migrations/init_market_manager_config.php
@@ -2,34 +2,106 @@
 
 /*
  * Fresns (https://fresns.org)
- * Copyright (C) 2021-Present Jarvis Tang
+ * Copyright (C) 2021-Present Jevan Tang
  * Released under the Apache-2.0 License.
  */
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+// use App\Utilities\ConfigUtility;
+// use App\Utilities\SubscribeUtility;
 use Illuminate\Support\Facades\Schema;
-use Plugins\MarketManager\Support\Installer;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Plugins\LaravelConfig\Utilities\ConfigUtility;
 
 return new class extends Migration
 {
+    protected $fresnsWordBody = [
+        // 'type' => SubscribeUtility::TYPE_USER_ACTIVITY,
+        // 'fskey' => 'market_manager',
+        // 'cmdWord' => 'stats',
+    ];
+
+    protected $fresnsConfigItems = [
+        // [
+        //     'item_tag' => 'market_manager',
+        //     'item_key' => 'access_key',
+        //     'item_type' => 'string',
+        //     'item_value' => null,
+        // ],
+        [
+            'item_tag' => 'market_manager',
+            'item_key' => 'market_server_host',
+            'item_type' => 'string',
+            'item_value' => 'https://marketplace.plugins-world.cn',
+        ],
+        [
+            'item_tag' => 'market_manager',
+            'item_key' => 'system_url',
+            'item_type' => 'string',
+            'item_value' => null,
+        ],
+        [
+            'item_tag' => 'market_manager',
+            'item_key' => 'settings_path',
+            'item_type' => 'string',
+            'item_value' => null,
+        ],
+        [
+            'item_tag' => 'market_manager',
+            'item_key' => 'install_datetime',
+            'item_type' => 'string',
+            'item_value' => null,
+        ],
+        [
+            'item_tag' => 'market_manager',
+            'item_key' => 'build_type',
+            'item_type' => 'number',
+            'item_value' => 1,
+        ],
+        [
+            'item_tag' => 'market_manager',
+            'item_key' => 'github_token',
+            'item_type' => 'string',
+            'item_value' => null,
+        ],
+    ];
+
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        (new Installer)->install();
+        foreach ($this->fresnsConfigItems as $index => $configItem) {
+            if ($configItem['item_key'] == 'install_datetime') {
+                $configItem['item_value'] = date('Y-m-d H:i:s');
+            }
+
+            if ($configItem['item_key'] == 'settings_path') {
+                $contents = file_get_contents(dirname(__DIR__, 2) . '/plugin.json');
+                $data = json_decode($contents, true);
+
+                $configItem['item_value'] = $data['settingsPath'];
+            }
+
+            $this->fresnsConfigItems[$index] = $configItem;
+        }
+
+        // addSubscribeItem
+        // \FresnsCmdWord::plugin()->addSubscribeItem($this->fresnsWordBody);
+
+        // addKeyValues to Config table
+        ConfigUtility::addFresnsConfigItems($this->fresnsConfigItems);
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        (new Installer)->uninstall();
+        // removeSubscribeItem
+        // \FresnsCmdWord::plugin()->removeSubscribeItem($this->fresnsWordBody);
+
+        // removeKeyValues from Config table
+        ConfigUtility::removeFresnsConfigItems($this->fresnsConfigItems);
     }
 };
